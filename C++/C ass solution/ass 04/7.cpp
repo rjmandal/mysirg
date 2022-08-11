867d969238139814e8652f120cad0b2a01bda9f1
sanjay mandal <rjsanjaymandal@gmail.com>
2022-08-11 18:12:47 +0530

    s

--- C++/C ass solution/ass 04/7.cpp
+++ C++/C ass solution/ass 04/7.cpp
@@ -3,16 +3,16 @@ using namespace std;
 int main()
 {
     system("cls");
-    int no,r=0,b=0;
+    int no,r=0,b=0,d=1;
     cout<<"enter the no ==>> ";
     cin>>no;
     while (no>0)
     {
-        r=r*10+no%2;
+        r=d*10+no%2;
         no=no/2;
     cout<<r;
     }
-    cout<<endl<<r<<endl;
+    cout<<endl<<r<<endl;        
     while(r>0)
     {
         b=b+r%10;
