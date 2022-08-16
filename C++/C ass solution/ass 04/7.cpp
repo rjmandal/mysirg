4459237a87ef06a4e2403a9999d63e2561e82094
sanjay <rjsanjaymandal@gmail.com>
2022-08-16 10:19:20 +0530

    s

--- C++/C ass solution/ass 04/7.cpp
+++ C++/C ass solution/ass 04/7.cpp
@@ -1,22 +1,36 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 int main()
 {
     system("cls");
-    int no,r=0,b=0,d=1;
-    cout<<"enter the no ==>> ";
-    cin>>no;
-    while (no>0)
+    int no, ans=0, r, i = 0;
+    cout << "enter the no ==>> ";
+    cin >> no;
+    while (no > 0)
     {
-        r=d*10+no%2;
-        no=no/2;
+        r = no % 2;
+        no = no / 2;
+
+        ans = r * pow(10, i) + ans;
+        i++;
     }
-    cout<<endl<<r<<endl;        
-    while(r>0)
-    {
-        b=b+r%10;
-        r=r/10;
-    }
-    cout<<r;
+    cout << endl
+         << ans;
+
+    // while (no>1)
+    // {
+    //     r=r*10+no%2;
+    //     no=no/2;
+    //     // cout<<r<<endl;
+    // }
+    // // cout<<endl<<r<<endl;
+    // while(r>1)
+    // {
+    //     b=b*10+r%10;
+    //     r=r/10;
+    //     // cout<<r<<endl;
+    // }
+    // cout<<endl<<b;
     return 0;
 }
---
+++ C++/C ass solution/ass 04/8.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int no, ans = 0, r, i = 0;
+    cout << "enter the no ==>> ";
+    cin >> no;
+    cout << endl
+         << no;
+}
